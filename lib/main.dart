a612b3a5601f2258caafd9eb00c450ce3d6ffac3
Jamoliddin developer <jamoliddinvahobov76@gmail.com>
2024-09-07 09:54:30 +0500

    step-12

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Beeline(),
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
     );
   }
 }
